2a6c72460b26d6f475fb61dbfad0d4dbcbcb232c
Alezin777 <alessandrogalvao37@gmail.com>
2024-05-23 20:17:32 -0300

    AAAIIIIII

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,15 +1,21 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView  bottomNavView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,37 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        //apresentando o xml para o java
+        bottomNavView = findViewById(R.id.bottomNavView);
+        //criando evento de click nos item de menu
+        bottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.mNews:
+                        Toast.makeText(getApplicationContext(),
+                                "Cliquei em News",Toast.LENGTH_SHORT).show();
+                        ;
+                    break;case R.id.mAdd:
+                        Toast.makeText(getApplicationContext(),
+                                "Cliquei em Add",Toast.LENGTH_SHORT).show();
+                        ;
+                    break;case R.id.mNewspaper:
+                        Toast.makeText(getApplicationContext(),
+                                "Cliquei em Newspaper",Toast.LENGTH_SHORT).show();
+                        ;
+                    break;case R.id.mPhone:
+                        Toast.makeText(getApplicationContext(),
+                                "Cliquei em Phone",Toast.LENGTH_SHORT).show();
+                        ;
+                    break;
+
+                }
+
+                return false;
+            }
         });
     }
 }
