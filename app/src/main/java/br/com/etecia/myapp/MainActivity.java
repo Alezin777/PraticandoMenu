184005448eefe81ac561fc8ae09d827c5c1a9f76
Alezin777 <alessandrogalvao37@gmail.com>
2024-05-23 21:46:04 -0300

    UUUUIII

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -7,15 +7,19 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView  bottomNavView;
+    BottomNavigationView bottomNavView;
+    MaterialToolbar TopBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,25 +32,60 @@ public class MainActivity extends AppCompatActivity {
         });
         //apresentando o xml para o java
         bottomNavView = findViewById(R.id.bottomNavView);
+        TopBar = findViewById(R.id.TopBar);
         //criando evento de click nos item de menu
+        TopBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.mFavorite) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei em Favorite", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mExplore) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei em Explore", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mCloud) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei em Cloud", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mArrow) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei em Arrow", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mSearch) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei em Search", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mNewspaper) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei em Newspaper", Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
         bottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 if (menuItem.getItemId() == R.id.mNews) {
                     Toast.makeText(getApplicationContext(),
-                            "Cliquei em News",Toast.LENGTH_SHORT).show();
+                            "Cliquei em News", Toast.LENGTH_SHORT).show();
 
-                }if (menuItem.getItemId() == R.id.mAdd) {
+                }
+                if (menuItem.getItemId() == R.id.mAdd) {
                     Toast.makeText(getApplicationContext(),
-                            "Cliquei em Add",Toast.LENGTH_SHORT).show();
+                            "Cliquei em Add", Toast.LENGTH_SHORT).show();
 
-                }if (menuItem.getItemId() == R.id.mNewspaper) {
+                }
+                if (menuItem.getItemId() == R.id.mNewspaper) {
                     Toast.makeText(getApplicationContext(),
-                            "Cliquei em Newspaper",Toast.LENGTH_SHORT).show();
+                            "Cliquei em Newspaper", Toast.LENGTH_SHORT).show();
 
-                }if (menuItem.getItemId() == R.id.mPhone) {
+                }
+                if (menuItem.getItemId() == R.id.mPhone) {
                     Toast.makeText(getApplicationContext(),
-                            "Cliquei em Phone",Toast.LENGTH_SHORT).show();
+                            "Cliquei em Phone", Toast.LENGTH_SHORT).show();
 
                 }
 
